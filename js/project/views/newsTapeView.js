9057b1c700a098f66256f5a660b7bce960940e86
zlodiak <prozaik81-2@yandex.ru>
2016-08-19 11:32:57 +0300

    n

--- js/project/views/newsTapeView.js
+++ js/project/views/newsTapeView.js
@@ -71,7 +71,8 @@ APP.NewsUnitView = Backbone.View.extend({
     console.log('mod');
 
     var newsModalView = new APP.NewsModalView(this.model);
-    $('body').append(newsModalView.render().el);
+    //$('body').append(newsModalView.render().el);
+    newsModalView.show(555, ['dfsdsdf']);
   }
 
 });
@@ -81,6 +82,8 @@ APP.NewsModalView = Backbone.View.extend({
 
   initialize: function(model) {   
     this.model = model;
+    _block = null;
+    _win = null;    
   },
 
   template: _.template($('#newsModalTpl').html()),
@@ -94,13 +97,56 @@ APP.NewsModalView = Backbone.View.extend({
     return this;
   },
 
-  events:{
-    'click': 'closeModal'
-  },  
+      initBlock: function() {
+              _block = document.getElementById('blockscreen'); //Получаем наш блокирующий фон по ID
 
-  closeModal: function() {
-    console.log('clo');
-  }
+              //Если он не определен, то создадим его
+              if (!_block) {
+                  var parent = document.getElementsByTagName('body')[0]; //Получим первый элемент тега body
+                  var obj = parent.firstChild; //Для того, чтобы вставить наш блокирующий фон в самое начало тега body
+                  _block = document.createElement('div'); //Создаем элемент div
+                  _block.id = 'blockscreen'; //Присваиваем ему наш ID
+                  parent.insertBefore(_block, obj); //Вставляем в начало
+                  _block.onclick = function() { this.close(); } //Добавим обработчик события по нажатию на блокирующий экран - закрыть модальное окно.
+              }
+              _block.style.display = 'inline'; //Установим CSS-свойство        
+          },
+
+       initWin: function(width, html) {
+              _win = document.getElementById('modalwindow'); //Получаем наше диалоговое окно по ID
+              //Если оно не определено, то также создадим его по аналогии
+              if (!_win) {
+                  var parent = document.getElementsByTagName('body')[0];
+                  var obj = parent.firstChild;
+                  _win = document.createElement('div');
+                  _win.id = 'modalwindow';
+                  _win.style.padding = '0 0 5px 0';
+                  parent.insertBefore(_win, obj);
+              }
+              _win.style.width = width + 'px'; //Установим ширину окна
+              _win.style.display = 'inline'; //Зададим CSS-свойство
+              
+              _win.innerHTML = html; //Добавим нужный HTML-текст в наше диалоговое окно
+              
+              //Установим позицию по центру экрана
+
+              _win.style.left = '50%'; //Позиция по горизонтали
+              _win.style.top = '50%'; //Позиция по вертикали
+
+              //Выравнивание по центру путем задания отрицательных отступов
+              _win.style.marginTop = -(_win.offsetHeight / 2) + 'px'; 
+              _win.style.marginLeft = -(width / 2) + 'px';
+          },
+
+       close: function() {
+              document.getElementById('blockscreen').style.display = 'none';
+              document.getElementById('modalwindow').style.display = 'none';        
+          },
+
+       show: function(html) {
+              this.initBlock();
+              this.initWin(html);
+          }
 
 });
 
