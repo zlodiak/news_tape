c4862c6b9aef6ed09f401ffe5f2d23b7c2cbea0f
zlodiak <prozaik81-2@yandex.ru>
2016-08-19 20:28:57 +0300

    try 2 fail

--- js/project/collections/collections.js
+++ js/project/collections/collections.js
@@ -1,3 +1,12 @@
 APP.NewsModelsCollection = Backbone.Collection.extend({
-  model: APP.NewsModel
+  model: APP.NewsModel,
+
+  search : function(letters){
+    if(letters == "") return this;
+    
+    var pattern = new RegExp(letters,"gi");
+    return _(this.filter(function(data) {
+        return pattern.test(data.get("name"));
+    }));
+  }  
 });
