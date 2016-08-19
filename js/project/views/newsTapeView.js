2c23f0c3c4d9aa4abaed092978a5000de6b16072
zlodiak <prozaik81-2@yandex.ru>
2016-08-19 12:43:36 +0300

    modal code beauty

--- js/project/views/newsTapeView.js
+++ js/project/views/newsTapeView.js
@@ -50,11 +50,8 @@ APP.NewsUnitView = Backbone.View.extend({
 
   initialize: function(model) {   
     self = this; 
-    this.model = model.model;
 
-    this.$el.on('click', function() {
-      self.openModal();
-    });    
+    this.model = model.model;
   },
 
   className: 'news',
@@ -70,13 +67,15 @@ APP.NewsUnitView = Backbone.View.extend({
     return this;
   },
 
+  events: {
+    'click': function() {
+      self.openModal();
+    }
+  },
+
   openModal: function() {
-    console.log('mod', this);
-
     var newsModalView = new APP.NewsModalView(this.model);
-    //$('body').append(newsModalView.render().el);
     newsModalView.show(555, ['dfsdsdf']);
-
   }
 
 });
@@ -90,10 +89,6 @@ APP.NewsModalView = Backbone.View.extend({
     this.model = model;
     _block = null;
     _win = null;    
-
-/*    $('#blockscreen').on('click', function() {
-      self.close();
-    });*/
   },
 
   template: _.template($('#newsModalTpl').html()),
@@ -109,59 +104,59 @@ APP.NewsModalView = Backbone.View.extend({
   },
 
   initBlock: function() {
-          var self = this;
-          
-          var _block = document.getElementById('blockscreen'); //Получаем наш блокирующий фон по ID
+    var self = this;
 
-          //Если он не определен, то создадим его
-          if (!_block) {
-              var parent = document.getElementsByTagName('body')[0]; //Получим первый элемент тега body
-              var obj = parent.firstChild; //Для того, чтобы вставить наш блокирующий фон в самое начало тега body
-              _block = document.createElement('div'); //Создаем элемент div
-              _block.id = 'blockscreen'; //Присваиваем ему наш ID
-              parent.insertBefore(_block, obj); //Вставляем в начало
-              _block.onclick = function() { console.log('close 1')
-                self.close(); 
-              } //Добавим обработчик события по нажатию на блокирующий экран - закрыть модальное окно.
-          }
-          _block.style.display = 'inline'; //Установим CSS-свойство        
-      },
+    var _block = document.getElementById('blockscreen'); //Получаем наш блокирующий фон по ID
 
-   initWin: function(width, html) {
-          _win = document.getElementById('modalwindow'); //Получаем наше диалоговое окно по ID
-          //Если оно не определено, то также создадим его по аналогии
-          if (!_win) {
-              var parent = document.getElementsByTagName('body')[0];
-              var obj = parent.firstChild;
-              _win = document.createElement('div');
-              _win.id = 'modalwindow';
-              _win.style.padding = '0 0 5px 0';
-              parent.insertBefore(_win, obj);
-          }
-          _win.style.width = width + 'px'; //Установим ширину окна
-          _win.style.display = 'inline'; //Зададим CSS-свойство
-          
-          _win.innerHTML = html; //Добавим нужный HTML-текст в наше диалоговое окно
-          
-          //Установим позицию по центру экрана
+    //Если он не определен, то создадим его
+    if (!_block) {
+        var parent = document.getElementsByTagName('body')[0]; //Получим первый элемент тега body
+        var obj = parent.firstChild; //Для того, чтобы вставить наш блокирующий фон в самое начало тега body
+        _block = document.createElement('div'); //Создаем элемент div
+        _block.id = 'blockscreen'; //Присваиваем ему наш ID
+        parent.insertBefore(_block, obj); //Вставляем в начало
+        _block.onclick = function() { 
+          self.close(); 
+        } //Добавим обработчик события по нажатию на блокирующий экран - закрыть модальное окно.
+    }
+    _block.style.display = 'inline'; //Установим CSS-свойство        
+  },
 
-          _win.style.left = '50%'; //Позиция по горизонтали
-          _win.style.top = '50%'; //Позиция по вертикали
+  initWin: function(width, html) {
+    _win = document.getElementById('modalwindow'); //Получаем наше диалоговое окно по ID
+    //Если оно не определено, то также создадим его по аналогии
+    if (!_win) {
+        var parent = document.getElementsByTagName('body')[0];
+        var obj = parent.firstChild;
+        _win = document.createElement('div');
+        _win.id = 'modalwindow';
+        _win.style.padding = '0 0 5px 0';
+        parent.insertBefore(_win, obj);
+    }
+    _win.style.width = width + 'px'; //Установим ширину окна
+    _win.style.display = 'inline'; //Зададим CSS-свойство
+    
+    _win.innerHTML = html; //Добавим нужный HTML-текст в наше диалоговое окно
+    
+    //Установим позицию по центру экрана
 
-          //Выравнивание по центру путем задания отрицательных отступов
-          _win.style.marginTop = -(_win.offsetHeight / 2) + 'px'; 
-          _win.style.marginLeft = -(width / 2) + 'px';
-      },
+    _win.style.left = '50%'; //Позиция по горизонтали
+    _win.style.top = '50%'; //Позиция по вертикали
 
-   close: function() {  console.log('close 2')
-          document.getElementById('blockscreen').style.display = 'none';
-          document.getElementById('modalwindow').style.display = 'none';        
-      },
+    //Выравнивание по центру путем задания отрицательных отступов
+    _win.style.marginTop = -(_win.offsetHeight / 2) + 'px'; 
+    _win.style.marginLeft = -(width / 2) + 'px';
+  },
 
-   show: function(html) {
-          this.initBlock();
-          this.initWin(html);
-      }
+  close: function() { 
+    document.getElementById('blockscreen').style.display = 'none';
+    document.getElementById('modalwindow').style.display = 'none';        
+  },
+
+  show: function(html) {
+    this.initBlock();
+    this.initWin(html);
+  }
 
 });
 
